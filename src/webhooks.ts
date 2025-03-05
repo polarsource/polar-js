6a16643a0d427e226ff61ed8ec392117e6ad3b29
François Voron <fvoron@gmail.com>
2025-03-05 17:45:15 +0100

    Add new webhooks to helper

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -1,8 +1,9 @@
-import { Webhook, WebhookVerificationError as _WebhookVerificationError } from "standardwebhooks"
-
 import {
-    WebhookBenefitCreatedPayload$inboundSchema
-} from './models/components/webhookbenefitcreatedpayload.js'
+  Webhook,
+  WebhookVerificationError as _WebhookVerificationError,
+} from "standardwebhooks";
+
+import { WebhookBenefitCreatedPayload$inboundSchema } from "./models/components/webhookbenefitcreatedpayload.js";
 import { WebhookBenefitGrantCreatedPayload$inboundSchema } from "./models/components/webhookbenefitgrantcreatedpayload.js";
 import { WebhookBenefitGrantRevokedPayload$inboundSchema } from "./models/components/webhookbenefitgrantrevokedpayload.js";
 import { WebhookBenefitGrantUpdatedPayload$inboundSchema } from "./models/components/webhookbenefitgrantupdatedpayload.js";
@@ -22,88 +23,104 @@ import { WebhookSubscriptionActivePayload$inboundSchema } from "./models/compone
 import { WebhookSubscriptionCanceledPayload$inboundSchema } from "./models/components/webhooksubscriptioncanceledpayload.js";
 import { WebhookSubscriptionCreatedPayload$inboundSchema } from "./models/components/webhooksubscriptioncreatedpayload.js";
 import { WebhookSubscriptionRevokedPayload$inboundSchema } from "./models/components/webhooksubscriptionrevokedpayload.js";
-import { WebhookSubscriptionUncanceledPayload$inboundSchema} from "./models/components/webhooksubscriptionuncanceledpayload.js";
+import { WebhookSubscriptionUncanceledPayload$inboundSchema } from "./models/components/webhooksubscriptionuncanceledpayload.js";
 import { WebhookSubscriptionUpdatedPayload$inboundSchema } from "./models/components/webhooksubscriptionupdatedpayload.js";
+import { WebhookCustomerCreatedPayload$inboundSchema } from "./models/components/webhookcustomercreatedpayload.js";
+import { WebhookCustomerUpdatedPayload$inboundSchema } from "./models/components/webhookcustomerupdatedpayload.js";
+import { WebhookCustomerDeletedPayload$inboundSchema } from "./models/components/webhookcustomerdeletedpayload.js";
+import { WebhookCustomerStateChangedPayload$inboundSchema } from "./models/components/webhookcustomerstatechangedpayload.js";
 import { SDKValidationError } from "./models/errors/sdkvalidationerror.js";
 
-
 class WebhookVerificationError extends Error {
-    constructor(message: string) {
-        super(message);
-        this.message = message;
-    }
+  constructor(message: string) {
+    super(message);
+    this.message = message;
+  }
 }
 
 const parseEvent = (parsed: any) => {
-    try {
-        switch (parsed.type) {
-            case 'benefit.created':
-                return WebhookBenefitCreatedPayload$inboundSchema.parse(parsed);
-            case 'benefit_grant.created':
-                return WebhookBenefitGrantCreatedPayload$inboundSchema.parse(parsed);
-            case 'benefit_grant.revoked':
-                return WebhookBenefitGrantRevokedPayload$inboundSchema.parse(parsed);
-            case 'benefit_grant.updated':
-                return WebhookBenefitGrantUpdatedPayload$inboundSchema.parse(parsed);
-            case 'benefit.updated':
-                return WebhookBenefitUpdatedPayload$inboundSchema.parse(parsed);
-            case 'checkout.created':
-                return WebhookCheckoutCreatedPayload$inboundSchema.parse(parsed);
-            case 'checkout.updated':
-                return WebhookCheckoutUpdatedPayload$inboundSchema.parse(parsed);
-            case 'order.created':
-                return WebhookOrderCreatedPayload$inboundSchema.parse(parsed);
-            case 'order.refunded':
-                return WebhookOrderRefundedPayload$inboundSchema.parse(parsed);
-            case 'organization.updated':
-                return WebhookOrganizationUpdatedPayload$inboundSchema.parse(parsed);
-            case 'pledge.created':
-                return WebhookPledgeCreatedPayload$inboundSchema.parse(parsed);
-            case 'pledge.updated':
-                return WebhookPledgeUpdatedPayload$inboundSchema.parse(parsed);
-            case 'product.created':
-                return WebhookProductCreatedPayload$inboundSchema.parse(parsed);
-            case 'product.updated':
-                return WebhookProductUpdatedPayload$inboundSchema.parse(parsed);
-            case 'refund.created':
-                return WebhookRefundCreatedPayload$inboundSchema.parse(parsed);
-            case 'refund.updated':
-                return WebhookRefundUpdatedPayload$inboundSchema.parse(parsed);
-            case 'subscription.active':
-                return WebhookSubscriptionActivePayload$inboundSchema.parse(parsed);
-            case 'subscription.canceled':
-                return WebhookSubscriptionCanceledPayload$inboundSchema.parse(parsed);
-            case 'subscription.created':
-                return WebhookSubscriptionCreatedPayload$inboundSchema.parse(parsed);
-            case 'subscription.revoked':
-                return WebhookSubscriptionRevokedPayload$inboundSchema.parse(parsed);
-            case 'subscription.uncanceled':
-                return WebhookSubscriptionUncanceledPayload$inboundSchema.parse(parsed);
-            case 'subscription.updated':
-                return WebhookSubscriptionUpdatedPayload$inboundSchema.parse(parsed);
-            default:
-                throw new SDKValidationError(`Unknown event type: ${parsed.type}`, parsed.type, parsed);
-        }
-    } catch (error) {
-        throw new SDKValidationError('Failed to parse event', error, parsed)
+  try {
+    switch (parsed.type) {
+      case "customer.created":
+        return WebhookCustomerCreatedPayload$inboundSchema.parse(parsed);
+      case "customer.updated":
+        return WebhookCustomerUpdatedPayload$inboundSchema.parse(parsed);
+      case "customer.deleted":
+        return WebhookCustomerDeletedPayload$inboundSchema.parse(parsed);
+      case "customer.state_changed":
+        return WebhookCustomerStateChangedPayload$inboundSchema.parse(parsed);
+      case "benefit.created":
+        return WebhookBenefitCreatedPayload$inboundSchema.parse(parsed);
+      case "benefit_grant.created":
+        return WebhookBenefitGrantCreatedPayload$inboundSchema.parse(parsed);
+      case "benefit_grant.revoked":
+        return WebhookBenefitGrantRevokedPayload$inboundSchema.parse(parsed);
+      case "benefit_grant.updated":
+        return WebhookBenefitGrantUpdatedPayload$inboundSchema.parse(parsed);
+      case "benefit.updated":
+        return WebhookBenefitUpdatedPayload$inboundSchema.parse(parsed);
+      case "checkout.created":
+        return WebhookCheckoutCreatedPayload$inboundSchema.parse(parsed);
+      case "checkout.updated":
+        return WebhookCheckoutUpdatedPayload$inboundSchema.parse(parsed);
+      case "order.created":
+        return WebhookOrderCreatedPayload$inboundSchema.parse(parsed);
+      case "order.refunded":
+        return WebhookOrderRefundedPayload$inboundSchema.parse(parsed);
+      case "organization.updated":
+        return WebhookOrganizationUpdatedPayload$inboundSchema.parse(parsed);
+      case "pledge.created":
+        return WebhookPledgeCreatedPayload$inboundSchema.parse(parsed);
+      case "pledge.updated":
+        return WebhookPledgeUpdatedPayload$inboundSchema.parse(parsed);
+      case "product.created":
+        return WebhookProductCreatedPayload$inboundSchema.parse(parsed);
+      case "product.updated":
+        return WebhookProductUpdatedPayload$inboundSchema.parse(parsed);
+      case "refund.created":
+        return WebhookRefundCreatedPayload$inboundSchema.parse(parsed);
+      case "refund.updated":
+        return WebhookRefundUpdatedPayload$inboundSchema.parse(parsed);
+      case "subscription.active":
+        return WebhookSubscriptionActivePayload$inboundSchema.parse(parsed);
+      case "subscription.canceled":
+        return WebhookSubscriptionCanceledPayload$inboundSchema.parse(parsed);
+      case "subscription.created":
+        return WebhookSubscriptionCreatedPayload$inboundSchema.parse(parsed);
+      case "subscription.revoked":
+        return WebhookSubscriptionRevokedPayload$inboundSchema.parse(parsed);
+      case "subscription.uncanceled":
+        return WebhookSubscriptionUncanceledPayload$inboundSchema.parse(parsed);
+      case "subscription.updated":
+        return WebhookSubscriptionUpdatedPayload$inboundSchema.parse(parsed);
+      default:
+        throw new SDKValidationError(
+          `Unknown event type: ${parsed.type}`,
+          parsed.type,
+          parsed,
+        );
     }
-}
+  } catch (error) {
+    throw new SDKValidationError("Failed to parse event", error, parsed);
+  }
+};
 
-const validateEvent = (body: string | Buffer, headers: Record<string, string>, secret: string) => {
-    const base64Secret = Buffer.from(secret, 'utf-8').toString('base64');
-    const webhook = new Webhook(base64Secret);
-    try {
-        const parsed = webhook.verify(body, headers);
-        return parseEvent(parsed);
-    } catch (error) {
-        if (error instanceof _WebhookVerificationError) {
-            throw new WebhookVerificationError(error.message);
-        }
-        throw error;
+const validateEvent = (
+  body: string | Buffer,
+  headers: Record<string, string>,
+  secret: string,
+) => {
+  const base64Secret = Buffer.from(secret, "utf-8").toString("base64");
+  const webhook = new Webhook(base64Secret);
+  try {
+    const parsed = webhook.verify(body, headers);
+    return parseEvent(parsed);
+  } catch (error) {
+    if (error instanceof _WebhookVerificationError) {
+      throw new WebhookVerificationError(error.message);
     }
-}
+    throw error;
+  }
+};
 
-export {
-    validateEvent,
-    WebhookVerificationError,
-}
+export { validateEvent, WebhookVerificationError };
